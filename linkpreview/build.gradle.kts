ecb2cae40b42d18c8bbb0b26d5c9c8d1e51f81b1
Waqas Tahir <wakaztahir@gmail.com>
2022-03-24 14:51:58 +0000

    updated

--- linkpreview/build.gradle.kts
+++ linkpreview/build.gradle.kts
@@ -75,14 +75,10 @@ afterEvaluate {
         repositories {
             maven {
                 name = "GithubPackages"
-                /** Configure path of your package repository on Github
-                 *  Replace GITHUB_USERID with your/organisation Github userID and REPOSITORY with the repository name on GitHub
-                 */
                 url = uri("https://maven.pkg.github.com/codeckle/kmp-link-preview")
 
                 runCatching {
                     credentials {
-                        /**Create github.properties in root project folder file with gpr.usr=GITHUB_USER_ID  & gpr.key=PERSONAL_ACCESS_TOKEN**/
                         username = (githubProperties["gpr.usr"] ?: System.getenv("GPR_USER")).toString()
                         password = (githubProperties["gpr.key"] ?: System.getenv("GPR_API_KEY")).toString()
                     }
